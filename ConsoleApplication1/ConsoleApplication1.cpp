6aef70abe3e2579b4ef46779ad11cc7ee8dfafbe
lyx <doo8opnewman@outlook.com>
2024-07-18 17:14:26 +0800

    添加项目文件。

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,43 @@
+﻿//杭州电子科技大学-1500
+
+#include<iostream>
+using namespace std;
+int dp[5001][1001];
+int len[5001];
+int main()
+{
+	int T;
+	cin >> T;
+	for (int i = 0; i < T; i++)
+	{
+		int k, n;//客人人数和筷子数量
+		cin >> k >> n;
+		k += 8;
+		for (int j = n; j >= 1; j--)
+		{
+			cin >> len[j];
+		}
+		for (int j = 1; j <= n; j++)
+		{
+			for (int s = 1; s <= k; s++)
+			{
+				if (j < s * 3)
+				{
+					dp[j][s] = 0x3f3f3f;
+				}
+			}
+		}
+		for (int j = 3; j <= n; j++)
+		{
+			for (int s = 1; s <= k; s++)
+			{
+				if (j >= s * 3)
+				{
+					dp[j][s] = min(dp[j - 1][s], dp[j - 2][s - 1] + (len[j] - len[j - 1]) * (len[j] - len[j - 1]));
+				}
+				else break;
+			}
+		}
+		cout << dp[n][k] << endl;
+	}
+}
---
+++ summer24.7.18/summer24.7.18.cpp
@@ -0,0 +1,153 @@
+﻿//逆波兰
+
+#include <iostream>
+#include <string>
+#include <stack>
+#include <vector>
+using namespace std;
+
+//转化为逆波兰表达式
+string trans(string& s)
+{
+	string operand;
+	stack<char> Operator;
+	int flag = 0;//记录括号优先级
+	for (const auto& e : s)//一个一个遍历
+	{
+		if (e == '(')
+		{
+			Operator.push(e);
+			flag = 1;
+			continue;
+		}
+		if (e == ')')
+		{
+			flag = 0;
+			while (Operator.top() != '(')
+			{
+				operand.push_back(Operator.top());
+				Operator.pop();
+			}
+			Operator.pop();
+			continue;
+		}
+		//操作符
+		if (e == '+' || e == '-' || e == '*' || e == '/')
+		{
+			if (flag == 1)//如果在括号里面
+			{
+				if (Operator.top() == '(')
+				{
+					Operator.push(e);
+				}
+				else if ((e == '*' || e == '/') && (Operator.top() == '+' || Operator.top() == '-'))
+				{
+					Operator.push(e);
+				}
+				else//操作符的优先级低于或等于栈顶操作符则出栈，直至遇到'('
+				{
+					while (Operator.top() != '(')
+					{
+						operand.push_back(Operator.top());
+						Operator.pop();
+					}
+					Operator.push(e);
+				}
+			}
+			else if (Operator.empty())//栈空就入栈
+			{
+				Operator.push(e);
+			}
+			//操作符的优先级高于栈顶操作符，入栈
+			else if ((e == '*' || e == '/') && (Operator.top() == '+' || Operator.top() == '-'))
+			{
+				Operator.push(e);
+			}
+			else//操作符的优先级低于或等于栈顶操作符则出栈，直至栈空或者优先级高于栈顶操作符
+			{
+				while (!Operator.empty())
+				{
+					operand.push_back(Operator.top());
+					Operator.pop();
+				}
+				Operator.push(e);
+			}
+		}
+		//操作数
+		else
+		{
+			operand.push_back(e);
+		}
+	}
+	while (!Operator.empty())//最后将剩余在栈里面的全部出栈
+	{
+		operand.push_back(Operator.top());
+		Operator.pop();
+	}
+	return operand;
+}
+
+int evalRPN(const string& s)
+{
+	stack<char> operand;
+	int left = 0, right = 0;
+	for (const auto& e : s)
+	{
+		if (e == '+' || e == '-' || e == '*' || e == '/')
+		{
+			switch (e)
+			{
+			case '+':
+				right = operand.top();
+				operand.pop();
+				left = operand.top();
+				operand.pop();
+				operand.push(left + right);
+				break;
+			case '-':
+				right = operand.top();
+				operand.pop();
+				left = operand.top();
+				operand.pop();
+				operand.push(left - right);
+				break;
+			case '*':
+				right = operand.top();
+				operand.pop();
+				left = operand.top();
+				operand.pop();
+				operand.push(left * right);
+				break;
+			case '/':
+				right = operand.top();
+				operand.pop();
+				left = operand.top();
+				operand.pop();
+				operand.push(left / right);
+				break;
+			}
+		}
+		else//操作数
+		{
+			operand.push(e - '0');
+		}
+	}
+	return operand.top();
+}
+
+int RPN(const string& str)
+{
+	//1.中缀表达式转化为后缀表达式
+	string s(str);
+	s = trans(s);
+	//2.后缀表达式计算答案
+	return evalRPN(s);
+}
+
+int main()
+{
+	string s("5*(3+5)/5-4/2");
+	int ret = RPN(s);
+	cout << "ret:" << ret << endl;
+	return 0;
+}
